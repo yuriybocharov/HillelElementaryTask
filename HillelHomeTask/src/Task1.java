f11bedcb1c0a1e5e71dd0724fc9ef1eba5f8b595
yuriybocharov <bocharov0682@gmail.com>
2017-08-06 14:34:02 +0300

    Домашнее задание

--- HillelHomeTask/src/Task1.java
+++ HillelHomeTask/src/Task1.java
@@ -1,5 +1,19 @@
-/**
- * Created by Ura on 04.08.2017.
- */
+import java.util.Scanner;
+
 public class Task1 {
-}
+
+        public static void main(String[] args) {
+
+            //Среднее арифметическое значение двух чисел.
+
+            Scanner scanner = new Scanner (System.in);
+            System.out.println ("Ведите числа: ");
+
+            double a = scanner.nextDouble ();
+            double b = scanner.nextDouble ();
+
+            double resalt = (a + b) / 2;
+            System.out.println (resalt);
+        }
+    }
+
