b47b54850c815df2e6b514d182adcdf40688a3ea
yuriybocharov <bocharov0682@gmail.com>
2017-08-11 17:40:54 +0300

    Домашнее задание

--- Task14/src/Task14.java
+++ Task14/src/Task14.java
@@ -4,6 +4,7 @@ public class Task14 {
 
         int[][] mass = new int[5][10];
         int[][] mass1 = new int[1][5];
+        int temp;
 
         double d = 0;
 
@@ -22,6 +23,7 @@ public class Task14 {
             }
             System.out.println ("");
         }
+        //Не совсем удобный но тоже рабочий способ.
         for (int n = 0; n < 1; n++) {
             for (int c = 0; c < 5; c++) {
                 for (int j = 0; j < 1; j++) {
@@ -35,6 +37,25 @@ public class Task14 {
         }
         System.out.println ();
         System.out.println ("Changed the first two columns in places:");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+
+                System.out.print (mass[i][j] + " ");
+
+            }
+            System.out.println ("");
+        }
+        //меняем местами правельно
+        for (int j = 1; j < 2; j++) {
+            for (int i = 0; i < 5; i++) {
+                temp = mass[i][1];
+                mass[i][1] = mass[i][2];
+                mass[i][2] = temp;
+            }
+        }
+
+        System.out.println ();
+        System.out.println ("And changed places again:");
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 10; j++) {
 
