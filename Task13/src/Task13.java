6904940a11261fd71ed8aa62e8ef080938a4de0d
yuriybocharov <30383383+yuriybocharov@users.noreply.github.com>
2017-08-18 20:11:27 +0300

    Update Task13.java

--- Task13/src/Task13.java
+++ Task13/src/Task13.java
@@ -4,8 +4,8 @@ public class Task13 {
     {
         int summ =0;
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < mass.length; i++) {
+            for (int j = 0; j < mass[0].length; j++) {
                 summ += mass[i][j];
             }
         }
@@ -15,8 +15,8 @@ public class Task13 {
     private static double average(int[][] mass)
     {
         double  count = 0;
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < mass.length; i++) {
+            for (int j = 0; j < mass[0].length; j++) {
                 count++;
             }
         }
@@ -28,8 +28,8 @@ public class Task13 {
     private static int min (int[][] mass)
     {
         int  min = mass[0][0];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < mass.length; i++) {
+            for (int j = 0; j < mass[0].length; j++) {
                 if( min > mass[i][j]) {min = mass[i][j];}
             }
         }
@@ -40,8 +40,8 @@ public class Task13 {
     private static int max (int[][] mass)
     {
         int  max = mass[0][0];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < mass.length; i++) {
+            for (int j = 0; j < mass[0].length; j++) {
                 if( max < mass[i][j]) {max = mass[i][j];}
             }
         }
@@ -52,7 +52,7 @@ public class Task13 {
     private static int NumberOfPositiveElements (int[][] mass)
     {
         int  count = 0;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < mass.length; i++) {
             for (int j = 0; j < 10; j++) {
                 if (mass[i][j] > 0) {count++;}
             }
@@ -63,8 +63,8 @@ public class Task13 {
     private static void  AmountPerLine (int[][] mass)
     {
         int  summ = 0;
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < mass.length; i++) {
+            for (int j = 0; j < mass[0].length; j++) {
                 summ+= mass[i][j];
             }
             System.out.println("line " + (i + 1) + ": " + summ);
@@ -81,16 +81,16 @@ public class Task13 {
 
         double d = 0;
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < mass.length; i++) {
+            for (int j = 0; j < mass[0].length; j++) {
                 d = Math.random()*40 - 21;
                 mass[i][j] = (int)d;
 
             }
         }
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < mass.length; i++) {
+            for (int j = 0; j < mass[0].length; j++) {
                 System.out.print(mass[i][j] + " ");
 
             }
