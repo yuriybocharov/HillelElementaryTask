f4d870183ce7f4fd30c06e4e73f82e776dfbe8d6
yuriybocharov <bocharov0682@gmail.com>
2017-11-11 22:32:20 +0200

    Домашнее задание

--- Task22/src/Main.java
+++ Task22/src/Main.java
@@ -3,10 +3,20 @@ public class Main {
     public static void main(String[] args) {
 
 
-       MyStack myStack = new MyStack ();
-        String s = "((())))))";
-        myStack.pushStack (s);
-        myStack.brackets ();
+        MyStack myStack1 = new MyStack ();
+        MyStack myStack2 = new MyStack ();
+        MyStack myStack3 = new MyStack ();
+        MyStack myStack4 = new MyStack ();
+
+        myStack1.pushStack ("((())))");
+        myStack2.pushStack ("((((((()))))))");
+        myStack3.pushStack ("((())))))");
+        myStack4.pushStack ("(((((( Ну если я правельнно понял задание))))");
+
+        myStack1.brackets ();
+        myStack2.brackets ();
+        myStack3.brackets ();
+        myStack4.brackets ();
     }
 
    }
